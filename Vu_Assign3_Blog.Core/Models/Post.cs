4b925f41dc50c02b4e87bbab9137e881a7bf809e
Long <vuminhl@sheridancollege.ca>
2025-11-18 14:38:01 -0500

    Create Post and Comment class in Core

---
+++ Vu_Assign3_Blog.Core/Models/Post.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vu_Assign3_Blog.Core.Models
+{
+    public class Post
+    {
+        public int Id{get;set;}
+        [Required]
+        [MaxLength(200)]
+        public string Title{get;set;}
+        [Required]
+        public string Content{get;set;}
+
+        public string Author{get;set;} = "admin";
+        public DateTime CreatedDate{get;set;} = DateTime.Now;
+        public DateTime? UpdatedDate{get;set;}
+        public IEnumerable<Comment> Comments {get;set;}
+    }
+}
