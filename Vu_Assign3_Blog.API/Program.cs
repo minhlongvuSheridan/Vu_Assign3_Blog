ff5ffbd93a4d907a4178ef317b784e0b63d3740b
Long <vuminhl@sheridancollege.ca>
2025-11-18 19:31:31 -0500

    add progam.cs, migrations, and

--- Vu_Assign3_Blog.API/Program.cs
+++ Vu_Assign3_Blog.API/Program.cs
@@ -1,24 +1,28 @@
+using Microsoft.EntityFrameworkCore;
 using Vu_Assign3_Blog.Core.Interfaces;
+using Vu_Assign3_Blog.Infrastructure.Data;
 using Vu_Assign3_Blog.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers().AddNewtonsoftJson();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<Assign3DbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+app.MapOpenApi();
 }
-
-
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
 
 
--- Vu_Assign3_Blog.Core/obj/Debug/net9.0/Vu_Assign3_Blog.Core.AssemblyInfo.cs
+++ Vu_Assign3_Blog.Core/obj/Debug/net9.0/Vu_Assign3_Blog.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vu_Assign3_Blog.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+306cc29b8d6ff1658c53682c8482b1320e28f8e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+514f0cf4a0a5fcf8dba7adca06e6425eef9bc66c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vu_Assign3_Blog.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vu_Assign3_Blog.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
